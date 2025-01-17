2f3b760d832b7ff84a54f4ce78df5522aea50acf
Герман Чердакли <g.cherdakly@int-sys.ru>
2025-01-17 14:13:59 +0300

    feat: migrated to new subroutines

--- src/main.go
+++ src/main.go
@@ -19,7 +19,6 @@ import (
 	// external
 	"github.com/hybridgroup/mjpeg"
 	"github.com/lmittmann/tint"
-	"gocv.io/x/gocv"
 	"golang.org/x/sync/errgroup"
 )
 
@@ -87,9 +86,8 @@ func main() {
 	ctx := context.Background()
 	eg, child_ctx := errgroup.WithContext(ctx)
 
-	stats_chan := make(chan struct{}, 8)
-
-	output_stream := mjpeg.NewStream()
+  // TODO: try buffering
+  frames_chan := make(chan []byte)
 
 	eg.Go(func() error {
 		return webserver(
@@ -100,18 +98,20 @@ func main() {
 	})
 
 	eg.Go(func() error {
-		return video(
-			child_ctx, logger,
-			rpath.Convert(exe_dir, cfg.Input.Path),
-			rpath.Convert(exe_dir, cfg.Model.Path),
-			output_stream, stats_chan)
+		// return video(
+		// 	child_ctx, logger,
+		// 	rpath.Convert(exe_dir, cfg.Input.Path),
+		// 	rpath.Convert(exe_dir, cfg.Model.Path),
+		// 	output_stream, stats_chan)
+    return processor(
+    child_ctx, logger, cfg, frames_chan)
 	})
 
-	eg.Go(func() error {
-		return stat(
-			child_ctx, logger, stats_chan,
-			cfg.Logging.StatPeriodSec)
-	})
+	// eg.Go(func() error {
+	// 	return stat(
+	// 		child_ctx, logger, stats_chan,
+	// 		cfg.Logging.StatPeriodSec)
+	// })
 
 	eg.Go(func() error {
 		return control(child_ctx, logger)
@@ -120,123 +120,6 @@ func main() {
 	eg.Wait()
 
 	logger.Info("Stopped")
-}
-
-func webserver(
-	ctx context.Context,
-	logger *slog.Logger,
-	stream *mjpeg.Stream,
-	port uint,
-	read_timeout_sec, write_timeout_sec, shutdown_timeout_sec uint,
-) error {
-
-	http.Handle("/", stream)
-
-	server := &http.Server{
-		Addr:         fmt.Sprintf("127.0.0.1:%d", port),
-		ReadTimeout:  time.Duration(read_timeout_sec) * time.Second,
-		WriteTimeout: time.Duration(write_timeout_sec) * time.Second,
-	}
-
-	err_chan := make(chan error)
-
-	go func() {
-		err_chan <- server.ListenAndServe()
-	}()
-
-	select {
-	case <-ctx.Done():
-		logger.Info("Webserver cancelled by context. Shutting down...", "timeout (sec)", shutdown_timeout_sec)
-		shutdown_context, cancel := context.WithTimeout(
-			context.Background(),
-			time.Second*time.Duration(shutdown_timeout_sec))
-		defer cancel()
-		shutdown_initiated_timestamp := time.Now()
-		err := server.Shutdown(shutdown_context)
-		logger.Info(
-			"Webserver shut down successfully", "shutdown duration (sec)",
-			time.Now().Sub(shutdown_initiated_timestamp).Seconds(), "error", err)
-		return context.Canceled
-	case err := <-err_chan:
-		logger.Error("Server error", "port", port, "error", err)
-		return err
-	}
-}
-
-func video(
-	ctx context.Context,
-	logger *slog.Logger,
-	file_path string,
-	model_path string,
-	output_stream *mjpeg.Stream,
-	stats chan<- struct{}) error {
-
-	logger.Info("Opening file", "address", file_path)
-
-	input_stream, err := gocv.VideoCaptureFile(file_path)
-	if err != nil {
-		logger.Error("Can't open stream", "address", file_path, "err", err)
-		return ERR_BAD_INPUT
-	}
-	defer input_stream.Close()
-
-	net := gocv.ReadNetFromONNX(model_path)
-	if net.Empty() {
-		logger.Error("Error reading network model")
-		return ERR_BAD_MODEL
-	}
-	defer net.Close()
-
-	outputNames := getOutputLayerNames(&net)
-	if len(outputNames) == 0 {
-		logger.Error("Error reading output layer names")
-		return ERR_BAD_MODEL
-	} else {
-		for _, name := range outputNames {
-			logger.Info("Model info", "outputNames", name)
-		}
-	}
-
-	if err := net.SetPreferableBackend(gocv.NetBackendType(gocv.NetBackendOpenVINO)); err != nil {
-		return ERR_CANT_SET_BACKEND
-	}
-	if err := net.SetPreferableTarget(gocv.NetTargetType(gocv.NetTargetCPU)); err != nil {
-		return ERR_CANT_SET_TARGET
-	}
-
-	img := gocv.NewMat()
-	defer img.Close()
-
-	logger.Info("Video loop started")
-	for {
-		select {
-		case <-ctx.Done():
-			logger.Info("Video cancelled by context")
-			return context.Canceled
-		default:
-			if !input_stream.Read(&img) {
-				logger.Error("Can't read next frame", "stream", file_path)
-				return ERR_STREAM_ENDED
-			}
-			if img.Empty() {
-				logger.Error("Empty frame received, skipping", "stream", file_path)
-				continue
-			}
-
-			detect(&net, &img, outputNames)
-
-			stats <- struct{}{}
-			buf, err := gocv.IMEncode(".jpg", img)
-			if err != nil {
-				logger.Error("Can't encode frame")
-				return err
-			}
-			data := make([]byte, buf.Len())
-			copy(data, buf.GetBytes()) // need to profile this and maybe not copy the entire frame every time
-			output_stream.UpdateJPEG(buf.GetBytes())
-			buf.Close()
-		}
-	}
 }
 
 func stat(ctx context.Context, logger *slog.Logger, stats <-chan struct{}, stat_period_sec uint) error {
